5753596ef81052912b26a1798a458f4e9724f9e6
gisha <52324253+gisha313@users.noreply.github.com>
2025-03-27 23:52:22 +0100

    Make removeFromArray and test it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    let new_arr = []
+    for (element of arr)
+        if (!values.includes(element))
+            new_arr.push(element)
+    return new_arr;
 };
 
 // Do not edit below this line
