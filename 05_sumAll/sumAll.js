6c0172e0de12adbf3e8ae28042c2b44979cfcd4f
gisha <52324253+gisha313@users.noreply.github.com>
2025-03-28 00:05:53 +0100

    Make sumAll and test it

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let start = a < b ? a : b;
+    let end = a > b ? a : b;
+    if (typeof start !== 'number' || typeof end !== 'number')
+        return 'ERROR';
+    if (start % 1 !== 0 || end % 1 !== 0 || start < 0 ||  end < 0)
+        return 'ERROR';
+    
+    return (end*(end+1)) / 2 - (start*(start-1)) / 2;
 };
 
 // Do not edit below this line
