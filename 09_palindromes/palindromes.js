862df2f2952b8869ee7051709d7876d149996443
gisha <52324253+gisha313@users.noreply.github.com>
2025-04-07 17:36:35 +0200

    Make palindrome and test it

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const allowed = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
+    str = str.toLowerCase().split('').filter(char => allowed.includes(char)).join('');
+    return str === str.split('').reverse().join('');
 };
 
 // Do not edit below this line
