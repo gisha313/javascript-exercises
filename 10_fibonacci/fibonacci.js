3e2c87cd23de45bf5a0999e8257c14ab9df8ac23
gisha <52324253+gisha313@users.noreply.github.com>
2025-04-07 17:55:04 +0200

    Make fibonacci and test it

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let last = 1;
+    let curr = 1;
+    n = Number(n);
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    if (n === 1 || n === 2) return 1;
 
+    let index = 2;
+    while (index < n){
+        let temp = curr;
+        curr = last + curr;
+        last = temp;
+        index++;
+    }
+
+    return curr
 };
 
 // Do not edit below this line
