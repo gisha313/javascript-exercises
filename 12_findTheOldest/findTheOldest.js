af4a8efb1ec2a80cb2d757c2089d23d7aefa4c80
gisha <52324253+gisha313@users.noreply.github.com>
2025-04-07 18:05:38 +0200

    Make findTheOldest and test it

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let currentAge = (person.yearOfDeath || (new Date).getFullYear()) - person.yearOfBirth;
+        let oldestAge = (oldest.yearOfDeath || (new Date).getFullYear()) - oldest.yearOfBirth;
+        return currentAge > oldestAge ? person : oldest
+    });
 };
 
 // Do not edit below this line
